115bd5ac3583a481952c2d0e991d7d063e0d77cc
elsholz <hendrik.lankers.hl@googlemail.com>
2022-07-28 11:52:49 +0200

    add setup file

---
+++ setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup
+
+setup(
+    name='Perfect Tree Finder',
+    version='0.0.1',
+    description="Small package for finding perfect trees within a binary tree. Can be used to find viable subnets for a set of ips for example.",
+    url='https://github.com/elsholz/pefect_tree_finder',
+    author='Hendrik Lankers',
+    author_email='hendrik.lankers.hl@googlemail.com',
+    license='MIT',
+    packages=['perfecttreefinder'],
+    requirements=[],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
